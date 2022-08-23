e57384df27d6a8f9e78c126a330fdd6e3dd82f91
alvaro <aacevedod@fen.uchile.cl>
2022-08-23 15:56:00 -0400

    Causalidad a la Granger, metodo alternativo v0

--- Script_Tarea2.m
+++ Script_Tarea2.m
@@ -1,4 +1,7 @@
 %% Inicialización
 Objeto = Tarea2('base_22.xls');
 %% Causalidad A la Granger
-p = Objeto.P1_RepAR(Objeto.raz_pi,15,10);
+[p,est_q] = Objeto.P1_RepAR(Objeto.raz_pi,15,10);
+
+%%
+Objeto.Causalidad_Granger(Objeto.raz_pi,1,Objeto.raz_y,5);
