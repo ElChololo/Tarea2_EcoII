a13f1d6e3a78967d07c6015c36123a214bdc265d
ElChololo <aacevedod@fen.uchile.cl>
2022-08-23 01:03:13 -0400

    Preg 1 . 0

--- Script_Tarea2.m
+++ Script_Tarea2.m
@@ -1,1 +1,4 @@
+%% Inicialización
 Objeto = Tarea2('base_22.xls');
+%% Causalidad A la Granger
+p = Objeto.P1_RepAR(Objeto.raz_pi,15,10);
--- Tarea2.m
+++ Tarea2.m
@@ -31,10 +31,52 @@ classdef Tarea2
             obj.raz_y = log(raz_y);
         end
         
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        function [p] = P1_RepAR(obj,serie,orden_max_ar,n_correlaciones)
+            %% Estimar la mejor representación AR de la serie          
+            for j=1:orden_max_ar
+                % Perder observaciones(MODIFICABLE SI SE NECESITAN LOS AR CON MISMO NUMERO DE OBS)
+                Var_Depe = serie(j:end);
+                obs =length(Var_Depe);
+                Regresores = obj.Get_Regresores(serie,j);
+                % Regresión AR(orden)
+                phis = (Regresores'*Regresores)\(Regresores'*serie);
+                resid = serie - Regresores*phis;
+                resid_2 = resid'*resid;
+                % Computar test errores ruido blanco
+                
+                Test_pass = obj.Test_Q(resid,resid_2,n_correlaciones,obs);
+                if Test_pass == 0
+                    p=j;
+                    break
+                end
+            end
+        end
+        
+        function Regresores = Get_Regresores(obj,serie,orden)
+            %% Objeto para crear regresores de estimaciones AR,incluyendo constante
+            obs = length(serie)-orden;
+            Regresores= zeros(obs,orden+1);
+            cte = ones(obs,1);
+            Regresores(:,1) = cte;
+            for i=1:orden
+               rezagos = circshift(serie,i);
+               Regresores(:,i+1) = rezagos(orden+1:end);
+            end
+        end
+        
+        function Boolean_Test = Test_Q(obj,resid,resid_2,n_correlaciones,obs)
+            %% Computar las correlaciones estimadas
+            % Armar matriz que albergara las m primeras correlaciones
+            vec_corr_est = zeros(n_correlaciones,1);
+            for j=1:n_correlaciones
+               vec_corr_est(j) =  (resid(j+1:end)' * resid(1:end-j)) / resid_2;
+            end
+            %Armar el estadístico Q
+            for k=1:n_correlaciones
+                vec_corr_est(j) = (vec_corr_est(j)^2) /  (obs-n_correlaciones);
+            end
+            est_Q = obs*(obs+2)*sum(vec_corr_est);
+            Boolean_Test = est_Q < chi2inv(0.975,2);
         end
     end
 end
